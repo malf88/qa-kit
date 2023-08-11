bcf193e55d61977271dad12c2365504eb47d1eb8
Marco Aurélio Lima Fernandes <malf88@gmail.com>
2023-08-10 23:32:03 -0300

    Configurado o vue para trabalhar com os módulos do sistema.

---
+++ app/Modules/GestaoProjetos/Views/Vue/components/Board.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {onMounted} from "vue";
+
+onMounted(() => {
+    console.log('teste');
+});
+</script>
+
+<template>
+    <h1>Teste</h1>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -14,9 +14,9 @@ import { createApp } from 'vue';
  */
 
 const app = createApp({});
-
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+//
+// import ExampleComponent from './components/ExampleComponent.vue';
+// app.component('example-component', ExampleComponent);
 
 /**
  * The following block of code may be used to automatically register your
@@ -26,9 +26,12 @@ app.component('example-component', ExampleComponent);
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
+Object.entries(import.meta.glob('./../../app/Modules/**/Views/Vue/components/*.vue', { eager: true }))
+    .forEach(([path, definition]) => {
+        app.component(path.split('/')
+            .pop()
+            .replace(/\.\w+$/, ''), definition.default);
+});
 
 /**
  * Finally, we will attach the application instance to a HTML element with
